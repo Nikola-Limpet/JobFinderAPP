4c8178af9c6b53d78b93e2a6421e5315d8d9391e
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-08-24 16:47:53 +0700

    added hero and nav

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import NavBar from './components/NavBar'; // Adjust the path as necessary
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar />
+      <Hero  
+        title= 'Find your most suitable job here!'
+        subtitle = "With Nick's Job Portal, you can find a wide range of job opportunities, from temporary work to permanent positions. For employers, we offer a range of services to help you find the right candidate for your company."
+      />
+      
+      <HomeCards />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
